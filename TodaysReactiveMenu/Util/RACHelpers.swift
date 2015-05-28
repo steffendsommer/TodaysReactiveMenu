438e0bcaf564ad90cabf138c5c01d47d74bc5e09
al_skipp <al_skipp@fastmail.fm>
2015-05-28 11:42:25 +0100

    Remove constraints to generic arguments

--- TodaysReactiveMenu/Util/RACHelpers.swift
+++ TodaysReactiveMenu/Util/RACHelpers.swift
@@ -6,7 +6,6 @@
 //  Copyright (c) 2015 steffendsommer. All rights reserved.
 //
 
-import Foundation
 import ReactiveCocoa
 
 
@@ -18,13 +17,14 @@ public func ignoreNil<T: AnyObject, E: Any>(signalProducer: SignalProducer<T?, E
 }
 
 public func ignoreError<T: Any, E: ErrorType>(signalProducer: SignalProducer<T, E>) -> SignalProducer<T, NoError> {
+public func ignoreError<T, E>(signalProducer: SignalProducer<T, E>) -> SignalProducer<T, NoError> {
     return signalProducer
         |> catch { _ in
             SignalProducer<T, NoError>.empty
         }
 }
 
-public func merge<T, E: ErrorType>(signals: [SignalProducer<T, E>]) -> SignalProducer<T, E> {
+public func merge<T, E>(signals: [SignalProducer<T, E>]) -> SignalProducer<T, E> {
     return SignalProducer<SignalProducer<T, E>, E>(values: signals)
-        |> flatten(FlattenStrategy.Merge)
+        |> flatten(.Merge)
 }
